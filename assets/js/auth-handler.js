dd110fbf87f81d8786295885920ff785cd976640
pasan <minjanaap.21@itfac.mrt.ac.lk>
2025-06-24 11:35:00 +0530

    feat(firebase/api) : set signup error - fix the error where the signup logic handles.

--- assets/js/auth-handler.js
+++ assets/js/auth-handler.js
@@ -24,11 +24,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const result = await signUpWithEmail(email, password, fullname);
-    if (result.success) {
+    if (result.user) {
       alert("Signup successful! You can now login.");
       showLogin();
     } else {
-      alert("Signup error: " + result.error);
+      alert("Signup error: " + JSON.stringify(result.error));
     }
   });
 
